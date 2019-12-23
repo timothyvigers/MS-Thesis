064ea343857f844bd805db9851e598b7bb19eb5e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2019-12-23 14:51:13 -0700

    Update candidate_selection_merged.R

--- code/candidate_selection_merged.R
+++ code/candidate_selection_merged.R
@@ -31,9 +31,9 @@ vitd <- read.csv("/home/biostats_share/Norris/data/metabolomics/vitD.bc.csv")
 metab_methyl_lin_mod <- function(metabolomics,methylation,metab_name,methyl_name){
   temp <- merge(metabolomics,methylation,by = "samplekey")
   # Linear models
-  out <- lapply(names(methylation)[1:5], function(x){
+  out <- lapply(names(methylation)[1:(ncol(methylation)-4)], function(x){
     base_form <- paste0(x,"~sex+age+platform")
-    metabs <- lapply(names(metabolomics)[2:6], function(y) {
+    metabs <- lapply(names(metabolomics)[2:ncol(metabolomics)], function(y) {
       form <- as.formula(paste0(base_form,"+",y))
       mod <- tryCatch(lme(form,random = ~1|samplekey,data = temp,na.action = na.omit),
                       message = function(m) NULL,warning = function(m) NULL,error = function(m) NULL)
