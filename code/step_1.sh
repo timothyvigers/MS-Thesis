0975b24b70f58ba373f561194eda72b4768c4968
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-01-29 12:11:09 -0700

    Update step_1.sh

--- code/step_1.sh
+++ code/step_1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Run scripts with time, low priority, no hangup.
-nohup bash -c 'time nice -n19 Rscript code/triad_selection_step_1_long_adj.R' &
-nohup bash -c 'time nice -n19 Rscript code/triad_selection_step_1_sv_adj.R' &
-nohup bash -c 'time nice -n19 Rscript code/triad_selection_step_1_sv_unadj.R' &
+nohup bash -c 'time nice -n19 Rscript triad_selection_step_1_long_adj.R' &
+nohup bash -c 'time nice -n19 Rscript triad_selection_step_1_sv_adj.R' &
+nohup bash -c 'time nice -n19 Rscript triad_selection_step_1_sv_unadj.R' &
