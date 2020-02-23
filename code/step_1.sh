6a140309aba81c4860573f659a0f84a10a8df313
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-02-23 16:05:35 -0700

    Update step_1.sh

--- code/step_1.sh
+++ code/step_1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 Rscript code/step_1/dataset_adj.R
 Rscript code/step_1/triad_selection_step_1_sv_adj.R
-#Rscript code/step_1/triad_selection_step_1_sv_unadj.R
+Rscript code/step_1/triad_selection_step_1_sv_unadj.R
