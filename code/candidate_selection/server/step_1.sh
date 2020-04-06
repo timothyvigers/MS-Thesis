2f33eb9752aa81fe82e118b36eb0e7460122554b
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-04-06 10:18:11 -0600

    Update step_1.sh

--- code/candidate_selection/server/step_1.sh
+++ code/candidate_selection/server/step_1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-Rscript code/step_1/dataset_adj.R
-Rscript code/step_1/triad_selection_step_1_sv_adj.R
-Rscript code/step_1/triad_selection_step_1_sv_unadj.R
+Rscript /step_1/triad_selection_step_1_sv_adj.R
+Rscript /step_1/triad_selection_step_1_sv_unadj.R
