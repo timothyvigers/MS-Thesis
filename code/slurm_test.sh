191a18fbac19cf7483689ab723f50756b58f62d6
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-05-08 09:27:00 -0600

    auto git

--- code/slurm_test.sh
+++ code/slurm_test.sh
@@ -3,4 +3,4 @@
 #SBATCH -n 5
 #SBATCH --mem=1000
 #SBATCH --job-name=tim_slurm_test
-candidate_selection/server/step_1/triad_selection_step_1_sv_unadj.R
+Rscript candidate_selection/server/step_1/triad_selection_step_1_sv_unadj.R
