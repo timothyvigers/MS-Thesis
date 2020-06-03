1a898c9ffba262a674d8e0f89b7d197cb1f77da2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-06-03 10:43:40 -0600

    reorganize code

---
+++ code/networks/cit.R
@@ -0,0 +1,29 @@
+library(cit)
+# Load list and data
+load("./data/networks/pair_list.Rdata")
+load("./data/networks/pair_data.Rdata")
+pair_data$T1Dgroup = factor(pair_data$T1Dgroup)
+# Run CIT package
+cits = apply(pairs, 1, function(x){
+  methyl = as.character(x["methyl"])
+  metab = as.character(x["metab"])
+  temp = pair_data[,c("T1Dgroup",methyl,metab)]
+  temp = temp[complete.cases(temp),]
+  y = as.numeric(temp$T1Dgroup) - 1
+  X = temp[,c(methyl,metab)]
+  cinf1 = cit.bp(L = X[,1],G = X[,2],T = y)
+  cinf2 = cit.bp(L = X[,2],G = X[,1],T = y)
+  p = min(cinf1$p_cit,cinf2$p_cit)
+  d = which.min(c(cinf1$p_cit,cinf2$p_cit))
+  d = ifelse(d == 1,">","<")
+  if (p < 0.05) {return(paste(methyl,d,metab,p))} else {NA}
+})
+# Convert to dataframe
+cits = as.data.frame(cits[!is.na(cits)])
+cits = separate(cits,"cits[!is.na(cits)]",c("methyl","direction","metab","cit p"),
+                sep = " ")
+cits$`cit p` = round(as.numeric(cits$`cit p`),3)
+cits = cits[order(cits$`cit p`),]
+rownames(cits) = 1:nrow(cits)
+# Write
+save(cits,file = "./data/networks/cits.Rdata")
---
+++ code/networks/mcmc.R
@@ -0,0 +1,50 @@
+library(rjags)
+# MCMC parameters
+n_adapt = 1000
+iter = 10000
+vars = c("alpha0","alpha","beta0","beta","gamma0","gamma")
+# All pairs from cit package
+# DIC for each model
+all_dics = apply(cits[1:10,],1,function(x){
+  methyl = as.character(x["methyl"])
+  metab = as.character(x["metab"])
+  temp = pair_data[,c("T1Dgroup",methyl,metab)]
+  temp = temp[complete.cases(temp),]
+  temp$T1Dgroup = ifelse(temp$T1Dgroup == "T1D control",0,1)
+  # data for jags
+  N = nrow(temp)
+  jags_data = list(t1d=c(temp[,"T1Dgroup"]),methyl=c(temp[,methyl]),
+                   metab=c(temp[,metab]),
+                   N=N)
+  # Test all structures
+  dics = lapply(paste0("struct",1:24), function(x){
+    mod = jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
+                     data = jags_data, n.adapt = n_adapt,n.chains = 2)
+    dic = dic.samples(mod,n.iter = iter,progress.bar="none")
+    return(round(sum(dic$deviance) + sum(dic$penalty),1))
+  })
+  return(unlist(dics))
+})
+save(all_dics,file = "./data/networks/all_dic.Rdata")
+# Samples for all models
+all_samples = apply(cits,1,function(x){
+  methyl = as.character(x["methyl"])
+  metab = as.character(x["metab"])
+  temp = pair_data[,c("T1Dgroup",methyl,metab)]
+  temp = temp[complete.cases(temp),]
+  temp$T1Dgroup = ifelse(temp$T1Dgroup == "T1D control",0,1)
+  # data for jags
+  N = nrow(temp)
+  jags_data = list(t1d=c(temp[,"T1Dgroup"]),methyl=c(temp[,methyl]),
+                   metab=c(temp[,metab]),
+                   N=N)
+  # Test all structures
+  samples = lapply(paste0("struct",1:24), function(x){
+    mod = jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
+                     data = jags_data, n.adapt = n_adapt,n.chains = 2)
+    coda = coda.samples(mod,variable.names = vars,n.iter = iter,progress.bar="none")
+    return(coda)
+  })
+  return(samples)
+})
+save(all_samples,file = "./data/networks/all_samples.Rdata")
