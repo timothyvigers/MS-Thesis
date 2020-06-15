95c2a4c76f104ef073949cf289c47a0e2ba38658
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-06-15 14:25:01 -0600

    Update mediation.R

--- code/networks/mediation.R
+++ code/networks/mediation.R
@@ -20,6 +20,7 @@ med = apply(cits,1,function(x){
   out.mod = glm(outform,family = "binomial",data = temp)
   medform = as.formula(paste(mediator,"~",exposure))
   med.mod = lm(medform,data = temp)
-  m = mediate(med.mod,out.mod,treat = exposure,mediator = mediator,
-              treat.value = 10,control.value = -10)
+  m = mediate(med.mod,out.mod,treat = exposure,mediator = mediator)
+  m1 = mediate(med.mod,out.mod,treat = exposure,mediator = mediator,
+               treat.value = -10,control.value = 10)
 })
