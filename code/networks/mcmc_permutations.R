d8f214dfc692daca32218034548ecbc8e0a5bed6
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-01 13:16:26 -0600

    Update mcmc_permutations.R

--- code/networks/mcmc_permutations.R
+++ code/networks/mcmc_permutations.R
@@ -5,7 +5,7 @@ setwd("/home/vigerst/MS-Thesis")
 load("./data/networks/pair_data.Rdata")
 load("./data/networks/cits.Rdata")
 # Permutation and MCMC parameters
-nsim = 1000
+nsim = 10
 n_adapt = 1000
 iter = 10000
 vars = c("alpha0","alpha","beta0","beta","gamma0","gamma")
@@ -15,34 +15,36 @@ cits = cits[!(duplicated(cits[,c("methyl","metab")])),]
 all_perms = apply(cits,1,function(x){
   methyl = as.character(x["methyl"])
   metab = as.character(x["metab"])
-  temp = pair_data[,c("T1Dgroup",methyl,metab)]
-  temp = temp[complete.cases(temp),]
-  temp$T1Dgroup = ifelse(temp$T1Dgroup == "T1D control",0,1)
-  N = nrow(temp)
+  pair = pair_data[,c("T1Dgroup",methyl,metab)]
+  pair = pair[complete.cases(pair),]
+  pair$T1Dgroup = ifelse(pair$T1Dgroup == "T1D control",0,1)
+  # Permutation datasets
+  dfs <- lapply(1:nsim,function(x){
+    set.seed(x)
+    df <- cbind.data.frame(pair$T1Dgroup,sample(pair[,methyl]),sample(pair[,metab]))
+    colnames(df) <- colnames(pair)
+    return(df)
+  })
   # Parallel - make cluster
-  no_cores = 10
+  no_cores = detectCores()
   cl <- makeCluster(no_cores,type = "FORK")
-  # Permutations
-  perm_structs = 
-    parLapply(cl=cl,1:nsim, function(x){
-      perm = sample(nrow(temp))
-      # data for jags
-      jags_data =
-        list(t1d=c(temp[perm,"T1Dgroup"]),methyl=c(temp[,methyl]),
-             metab=c(temp[,metab]),
-             N=N)
-      dics =
-        suppressWarnings(lapply(paste0("struct",1:24), function(x){
-          mod =
-            jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
-                       data = jags_data, n.adapt = n_adapt,n.chains = 2)
-          dic =
-            dic.samples(mod,n.iter = iter,progress.bar="none")
-          return(round(sum(dic$deviance) + sum(dic$penalty),1))
-        }))
-      unlist(dics)
-    })
+  # MCMC on each permuted set
+  mcmc_perms <- parLapply(cl=cl,dfs,function(x){
+    jags_data =
+      list(t1d=c(x[,"T1Dgroup"]),methyl=c(x[,methyl]),
+           metab=c(x[,metab]),N=nrow(pair))
+    dics =
+      suppressWarnings(lapply(paste0("struct",1:24), function(x){
+        mod =
+          jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
+                     data = jags_data, n.adapt = n_adapt,n.chains = 2)
+        dic =
+          dic.samples(mod,n.iter = iter,progress.bar="none")
+        return(round(sum(dic$deviance) + sum(dic$penalty),1))
+      }))
+    unlist(dics)
+  })
   stopCluster(cl)
-  return(perm_structs)
+  return(mcmc_perms)
 })
-save(all_perms,file = "./data/networks/mcmc_perms.Rdata")
+save(all_perms,file = "./data/networks/all_mcmc_perms.Rdata")
