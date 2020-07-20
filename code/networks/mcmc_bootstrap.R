6fb94d844ce697978f0ec370c0e406055f53641e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-20 13:33:24 -0600

    Update mcmc_bootstrap.R

--- code/networks/mcmc_bootstrap.R
+++ code/networks/mcmc_bootstrap.R
@@ -13,10 +13,10 @@ vars = c("alpha0","alpha","beta0","beta","gamma0","gamma")
 cits = cits[!(duplicated(cits[,c("methyl","metab")])),]
 # Get DIC function
 get_dic <- function(methyl,metab,data = pair_data,indices) {
-  pair = data[,c("T1Dgroup",methyl,metab)]
-  pair = pair[complete.cases(pair),]
-  pair$T1Dgroup = ifelse(pair$T1Dgroup == "T1D control",0,1)
-  d <- pair[indices,] 
+  # pair = data[,c("T1Dgroup",methyl,metab)]
+  # pair = pair[complete.cases(pair),]
+  # pair$T1Dgroup = ifelse(pair$T1Dgroup == "T1D control",0,1)
+  d <- data[indices,] 
   
   jags_data =
     list(t1d=c(d[,"T1Dgroup"]),methyl=c(d[,methyl]),
@@ -34,5 +34,5 @@ get_dic <- function(methyl,metab,data = pair_data,indices) {
   return(unlist(dics))
 }
 # Bootstrap
-test <- boot(data = pair_data,statistic = get_dic,R=10,
+test <- boot(data = pair,statistic = get_dic,R=10,parallel = "multicore",
              methyl = "cg01793374",metab = "gctof_235")
