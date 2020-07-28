ecdc31ab9c000f15a59afffa86c50fc2e54849f5
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-07-28 09:43:37 -0600

    deal finally sort of working

--- code/networks/deal.R
+++ code/networks/deal.R
@@ -1,6 +1,6 @@
 library(deal)
 # Load data
-setwd("/Users/timvigers/GitHub/MS-Thesis/")
+setwd("/Users/timvigers/Documents/GitHub/MS-Thesis/")
 load("./data/networks/pair_data.Rdata")
 load("./data/networks/cits.Rdata")
 # Unique pairs from cit package
@@ -13,6 +13,14 @@ metab = as.character(x["metab"])
 pair = pair_data[,c("T1Dgroup",methyl,metab)]
 pair = pair[complete.cases(pair),]
 pair$T1Dgroup <- factor(pair$T1Dgroup,levels = c("T1D control","T1D case"))
+pair[,2:3] <- lapply(pair[,2:3],as.numeric)
 # Deal package
-net <- network(pair,specifygraph=FALSE,inspectprob=FALSE,doprob=TRUE)
-l <- learn(net,pair)
+# Make an empty network
+net <- network(pair)
+# Calculate joint prior
+net_prior <- jointprior(net) 
+# Learn parameters to get score for empty network
+learned <- learn(net,pair)$nw
+# Greedy search
+search <- autosearch(learned,pair)
+
