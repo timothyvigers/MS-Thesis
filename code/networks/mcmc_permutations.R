96a7c061c61f8295a40c2cae6e0450f04f9d64c7
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-08-02 10:51:45 -0600

    all pairs instead of just cit pairs

--- code/networks/mcmc_permutations.R
+++ code/networks/mcmc_permutations.R
@@ -3,16 +3,14 @@ library(parallel)
 # Load data
 setwd("/home/vigerst/MS-Thesis")
 load("./data/networks/pair_data.Rdata")
-load("./data/networks/cits.Rdata")
+load("./data/networks/pair_list.Rdata")
 # Permutation and MCMC parameters
-nsim = 100
+nsim = 1000
 n_adapt = 1000
 iter = 10000
 vars = c("alpha0","alpha","beta0","beta","gamma0","gamma")
-# Unique pairs from cit package
-cits = cits[!(duplicated(cits[,c("methyl","metab")])),]
 # DIC for each model with permutation tests
-all_perms = apply(cits,1,function(x){
+all_perms = apply(pairs,1,function(x){
   methyl = as.character(x["methyl"])
   metab = as.character(x["metab"])
   pair = pair_data[,c("T1Dgroup",methyl,metab)]
