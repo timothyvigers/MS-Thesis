1f5f65cc96b36f4e678a3e5c574f8d8d533ca25a
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-08-17 11:32:32 -0600

    Update slurm_make_datasets.sh

--- code/networks/slurm_make_datasets.sh
+++ code/networks/slurm_make_datasets.sh
@@ -2,3 +2,4 @@
 #SBATCH --job-name=tim_scaled_datasets
 #SBATCH --output=tim_scaled_datasets.log
 Rscript $HOME/MS-Thesis/code/networks/make_network_dataset.R
+Rscript $HOME/MS-Thesis/code/networks/make_all_dataset.R
