50bfb878e610f9d0199687545a5738ec9f68b1eb
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-08-19 12:53:15 -0600

    Update mcmc_permutations_random_pairs.R

--- code/networks/mcmc_permutations_random_pairs.R
+++ code/networks/mcmc_permutations_random_pairs.R
@@ -1,30 +1,45 @@
 library(rjags)
 library(parallel)
-# Load data
-setwd("/home/vigerst/MS-Thesis")
-load("./data/networks/pair_data.Rdata")
-load("./data/networks/pair_list.Rdata")
-load("./data/networks/cits.Rdata")
-load("./data/networks/all_data.Rdata")
 set.seed(1017)
+setwd("/home/vigerst/MS-Thesis")
+# Load data
+load("./data/networks/all_data.Rdata")
+# Methyl and metabolite associations
+gctof_step1 = read.csv("./data/candidate_selection/step_1/sv/gctof_SV_unadj_scaled.csv",stringsAsFactors = F)
+hilic_step1 = read.csv("./data/candidate_selection/step_1/sv/hilic_SV_unadj_scaled.csv",stringsAsFactors = F)
+lipid_step1 = read.csv("./data/candidate_selection/step_1/sv/lipid_SV_unadj_scaled.csv",stringsAsFactors = F)
+oxylipin_step1 = read.csv("./data/candidate_selection/step_1/sv/oxylipin_SV_unadj_scaled.csv",stringsAsFactors = F)
+vitd_step1 = read.csv("./data/candidate_selection/step_1/sv/vitd_SV_unadj_scaled.csv",stringsAsFactors = F)
+step1 = rbind(gctof_step1,hilic_step1)
+step1 = rbind(step1,lipid_step1)
+step1 = rbind(step1,oxylipin_step1)
+step1 = rbind(step1,vitd_step1)
+step1 = metab_step3[order(metab_step3$p.value),]
+# Import methylation association with T1D
+methyl_step2 = read.csv("./data/candidate_selection/step_2/methyl_adj.csv",stringsAsFactors = F)
+# Import metabolite associations with T1D
+gctof_step3 = read.csv("./data/candidate_selection/step_3/gctof_adj.csv",stringsAsFactors = F)
+hilic_step3 = read.csv("./data/candidate_selection/step_3/hilic_adj.csv",stringsAsFactors = F)
+lipid_step3 = read.csv("./data/candidate_selection/step_3/lipid_adj.csv",stringsAsFactors = F)
+oxylipin_step3 = read.csv("./data/candidate_selection/step_3/oxylipin_adj.csv",stringsAsFactors = F)
+vitd_step3 = read.csv("./data/candidate_selection/step_3/vitd_adj.csv",stringsAsFactors = F)
+metab_step3 = rbind(gctof_step3,hilic_step3)
+metab_step3 = rbind(metab_step3,lipid_step3)
+metab_step3 = rbind(metab_step3,oxylipin_step3)
+metab_step3 = rbind(metab_step3,vitd_step3)
+metab_step3 = metab_step3[order(metab_step3$p.value),]
+# Get totally unrelated pairs
+pairs = step1[step1$p.value >= 0.5,]
+pairs = pairs[pairs$methyl %in% methyl_step2$methyl[methyl_step2$p.value >= 0.5],]
+pairs = pairs[pairs$metab %in% metab_step3$metab[metab_step3$p.value >= 0.5],]
+subset_n = 139
+pairs = pairs[sample(1:nrow(pairs),subset_n),]
+# DIC for each model with permutation tests 
 # Permutation and MCMC parameters
-nsim = 100
+nsim = 1
 n_adapt = 1000
 iter = 10000
 vars = c("alpha0","alpha","beta0","beta","gamma0","gamma")
-# DIC for each model with permutation tests 
-# Get methyl and metabs that weren't in the pairs we selected
-methyls <- colnames(all_data)[(!(colnames(all_data) %in% pairs$methyl))]
-methyls <- methyls[grep("cg.*",methyls)]
-metabs <- colnames(all_data)[!(colnames(all_data) %in% pairs$metab)]
-metabs <- metabs[18:length(metabs)]
-metabs <- metabs[grep("cg.*",metabs,invert = T)]
-metabs <- metabs[grep("ch.*",metabs,invert = T)]
-# Get data
-subset_n = 139
-pairs = pairs[1:subset_n,]
-pairs$methyl <- sample(methyls,subset_n)
-pairs$metab <- sample(metabs,subset_n)
 # Iterate
 all_perms = apply(pairs,1,function(x){
   methyl = as.character(x["methyl"])
