a82ce2b2198a99c62b877a2db376a2f8a408808a
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-08-19 18:12:31 -0600

    Update slurm_mcmc_perm.sh

--- code/networks/slurm_mcmc_perm.sh
+++ code/networks/slurm_mcmc_perm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #SBATCH -n 20
-#SBATCH --job-name=tim_mcmc
-#SBATCH --output=tim_mcmc.log
+#SBATCH --job-name=tim_mcmc_unrelated_scaled
+#SBATCH --output=tim_mcmc_unrelated_scaled.log
 Rscript $HOME/MS-Thesis/code/networks/mcmc_permutations_random_pairs.R
