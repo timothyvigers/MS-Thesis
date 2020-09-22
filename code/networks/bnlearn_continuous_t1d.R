1c246f108ee48985e853cb5bc4465dc1fa1f842e
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-09-22 12:49:02 -0600

    Update bnlearn_continuous_t1d.R

--- code/networks/bnlearn_continuous_t1d.R
+++ code/networks/bnlearn_continuous_t1d.R
@@ -1,6 +1,6 @@
 library(bnlearn)
 # Load data
-setwd("C:/Users/timbv/Documents/GitHub/MS-Thesis/")
+setwd("/home/vigerst/MS-Thesis")
 load("./data/networks/pair_data.Rdata")
 load("./data/networks/pair_list.Rdata")
 load("./data/networks/cits.Rdata")
@@ -13,9 +13,10 @@ all_learned = apply(pairs,1,function(x){
   metab = as.character(x["metab"])
   pair = pair_data[,c("T1Dgroup",methyl,metab)]
   pair = pair[complete.cases(pair),]
-  pair$T1Dgroup = pair$T1Dgroup + rnorm(nrow(pair),sd = 0.0001)
-  d <- discretize(pair,breaks = 5,method = "interval")
-  learned <- hc(d,score = "aic")
+  pair$T1Dgroup[pair$T1Dgroup == "T1D control"] = 0
+  pair$T1Dgroup[pair$T1Dgroup == "T1D case"] = 1
+  pair$T1Dgroup = as.numeric(pair$T1Dgroup) + rnorm(nrow(pair),sd = 0.0001)
+  learned <- hc(pair)
   return(learned)
 })
 save(all_learned,file = "./data/networks/bnlearn_continuous_t1d.Rdata")
@@ -26,9 +27,10 @@ cit_learned = apply(cits,1,function(x){
   metab = as.character(x["metab"])
   pair = pair_data[,c("T1Dgroup",methyl,metab)]
   pair = pair[complete.cases(pair),]
-  pair$T1Dgroup = factor(pair$T1Dgroup)
-  d <- discretize(pair,breaks = 5,method = "interval")
-  learned <- hc(d,score = "aic")
+  pair$T1Dgroup[pair$T1Dgroup == "T1D control"] = 0
+  pair$T1Dgroup[pair$T1Dgroup == "T1D case"] = 1
+  pair$T1Dgroup = as.numeric(pair$T1Dgroup) + rnorm(nrow(pair),sd = 0.0001)
+  learned <- hc(pair)
   return(learned)
 })
-save(cit_learned,file = "./data/networks/bnlearn_cit_discretized.Rdata")
+save(cit_learned,file = "./data/networks/bnlearn_cit_continuous_t1d.Rdata")
