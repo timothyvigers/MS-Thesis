fc691dfbc2e8e7b754d50c31022a7dafed4c6046
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-10-01 14:08:57 -0600

    Update r2jags_structures.R

--- code/networks/r2jags_structures.R
+++ code/networks/r2jags_structures.R
@@ -1,5 +1,5 @@
 # Jags structures for R2jags
-
+#1
 struct1_jags <- function(){
   for (i in 1:N) {
     # metab | T1D
@@ -19,4 +19,44 @@ struct1_jags <- function(){
   tau ~ dgamma(0.0001, 0.0001)
   sigma2 <- 1/tau
 }
-
+# 2
+struct2_jags <- function(){
+  for (i in 1:N) {
+    # methyl | T1D
+    methyl[i] ~ dnorm(beta0 + beta*t1d[i], tau)
+    # metab | T1D, methyl
+    metab[i] ~ dnorm(int + alpha*t1d[i] + gamma*methyl[i], tau)
+    # Log likelihood for the network
+    LogLik[i] <- log(dnorm(metab[i],int + alpha*t1d[i] + gamma*methyl[i], tau))
+  }
+  # Very flat priors
+  alpha ~ dnorm(0,0.001)
+  beta0 ~ dnorm(0,0.001)
+  beta ~ dnorm(0,0.001)
+  int ~ dnorm(0,0.001)
+  gamma ~ dnorm(0,0.001)
+  
+  tau ~ dgamma(0.0001, 0.0001)
+  sigma2 <- 1/tau
+}
+# 3
+struct3_jags <- function(){
+  for (i in 1:N) {
+    # metab | methyl
+    metab[i] ~ dnorm(gamma0 + gamma*methyl[i], tau)
+    # T1D | methyl, metab
+    t1d[i] ~ dbern(q[i])
+    logit(q[i]) <- int + alpha*metab[i] + beta*methyl[i]
+    # Log likelihood for the network
+    #LogLik[i] <- log(dbern(t1d[i],logit(int + alpha*t1d[i] + gamma*methyl[i]), tau))
+  }
+  # Very flat priors
+  int ~ dnorm(0,0.001)
+  alpha ~ dnorm(0,0.001)
+  beta ~ dnorm(0,0.001)
+  gamma0 ~ dnorm(0,0.001)
+  gamma ~ dnorm(0,0.001)
+  
+  tau ~ dgamma(0.0001, 0.0001)
+  sigma2 <- 1/tau
+}
