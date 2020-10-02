85aa708a72550786b1a86e21ce3853e399803784
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-10-01 18:15:39 -0600

    Update r2jags_waic.R

--- code/networks/r2jags_waic.R
+++ code/networks/r2jags_waic.R
@@ -5,18 +5,15 @@ setwd("C:/Users/timbv/Documents/GitHub/MS-Thesis")
 load("./data/networks/pair_data.Rdata")
 load("./data/networks/cits.Rdata")
 set.seed(1017)
-# MCMC parameters
+# Permutation and MCMC parameters
+nsim = 100
 n_adapt = 1000
 iter = 10000
 vars = c("alpha0","alpha","beta0","beta","gamma0","gamma","LogLik")
 # Unique pairs from cit package
 cits = cits[!(duplicated(cits[,c("methyl","metab")])),]
-# Parallel
-# Make cluster
-no_cores = 10
-cl <- makeCluster(no_cores,type = "FORK")
 # DIC for each model
-all_dics = parApply(cl=cl,cits,1,function(x){
+all_waics = apply(cits[1:5,],1,function(x){
   methyl = as.character(x["methyl"])
   metab = as.character(x["metab"])
   temp = pair_data[,c("T1Dgroup",methyl,metab)]
@@ -26,42 +23,18 @@ all_dics = parApply(cl=cl,cits,1,function(x){
   jags_data = list(t1d=c(temp[,"T1Dgroup"]),methyl=c(temp[,methyl]),
                    metab=c(temp[,metab]),
                    N=N)
-  # R2jags
-  fit_lm1 <- jags(data = jags_data, 
-                  parameters.to.save = vars, 
-                  model.file = struct3_jags,
-                  n.chains = 2, n.iter = 10000, n.burnin = 1000)
-  # data for jags
- 
-  # Test all structures
-  dics = lapply(paste0("struct",1:24), function(x){
-    mod = jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
-                     data = jags_data, n.adapt = n_adapt,n.chains = 2)
-    dic = dic.samples(mod,n.iter = iter,progress.bar = "none")
-    return(round(sum(dic$deviance) + sum(dic$penalty),1))
+  # R2jags - try all structures
+  waics <- lapply(paste0("struct",1:3), function(w){
+    j <- get(paste0(w,"_jags"))
+    fit_lm <- suppressWarnings(
+      jags(data = jags_data,
+           parameters.to.save = vars,model.file = j,
+           n.chains = 2, n.iter = iter, n.burnin = n_adapt))
+    loglik <- fit_lm$BUGSoutput$sims.list$LogLik
+    waic <- suppressWarnings(waic(loglik))
+    loo <- suppressWarnings(loo(loglik))
+    return(c(waic$estimates["waic",1],loo$estimates["looic",1]))
   })
-  return(unlist(dics))
+  return(waics)
 })
-save(all_dics,file = "./data/networks/all_dic.Rdata")
-# Samples for all models
-all_samples = parApply(cl=cl,cits,1,function(x){
-  methyl = as.character(x["methyl"])
-  metab = as.character(x["metab"])
-  temp = pair_data[,c("T1Dgroup",methyl,metab)]
-  temp = temp[complete.cases(temp),]
-  temp$T1Dgroup = ifelse(temp$T1Dgroup == "T1D control",0,1)
-  # data for jags
-  N = nrow(temp)
-  jags_data = list(t1d=c(temp[,"T1Dgroup"]),methyl=c(temp[,methyl]),
-                   metab=c(temp[,metab]),
-                   N=N)
-  # Test all structures
-  samples = lapply(paste0("struct",1:24), function(x){
-    mod = jags.model(paste0("./code/jags/",x,".jags"),quiet=T,
-                     data = jags_data, n.adapt = n_adapt,n.chains = 2)
-    coda = coda.samples(mod,variable.names = vars,n.iter = iter,progress.bar="none")
-    return(coda)
-  })
-  return(samples)
-})
-save(all_samples,file = "./data/networks/all_samples.Rdata")
+save(all_waics,file = "./data/networks/all_waic.Rdata")
