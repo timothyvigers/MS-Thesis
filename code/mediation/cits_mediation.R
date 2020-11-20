42867aaae4ca39c300dde246228999b336a637d2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2020-11-20 15:48:35 -0700

    Update cits_mediation.R

--- code/mediation/cits_mediation.R
+++ code/mediation/cits_mediation.R
@@ -1,15 +1,15 @@
 library(mediation)
 # Load data
-setwd("C:/Users/tim/Documents/GitHub/MS-Thesis")
+setwd("/Users/timvigers/GitHub/MS-Thesis")
 load("./data/networks/all_data_methyl_scaled.Rdata")
 load("./data/networks/cits.Rdata")
 set.seed(1017)
 # Mediation model lists
 out_list = unique(paste0("factor(T1Dgroup)~",cits$methyl,"+",cits$metab))
 # Iterate through all
-cits_mediation = lapply(out_list, function(t){
+cits_mediation = lapply(out_list[1], function(t){
   split = strsplit(t,"\\~|\\+")
-  y = glm(as.formula(t),data = all_data_methyl_scaled,family = "binomial")
+  y1 = glm(as.formula(t),data = all_data_methyl_scaled,family = binomial("probit"))
   # methyl mediator
   m1_form = as.formula(paste0(split[[1]][2],"~",split[[1]][3]))
   m = lm(m1_form,data = all_data_methyl_scaled)
@@ -19,5 +19,6 @@ cits_mediation = lapply(out_list, function(t){
   m = lm(m2_form,data = all_data_methyl_scaled)
   med2 = mediate(m,y,mediator = split[[1]][3],treat = split[[1]][2])
   # Results - what to report here?
+  list(summary(med1),summary(med2))
 }) 
 save(cits_mediation,file = "./data/mediation/cits_mediation.Rdata")
