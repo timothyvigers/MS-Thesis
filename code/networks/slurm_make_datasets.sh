51648acff2a05ba390006946c60527f39577d5fb
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-01-18 15:44:16 -0700

    slurm

--- code/networks/slurm_make_datasets.sh
+++ code/networks/slurm_make_datasets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --job-name=tim_datasets
-#SBATCH --output=make_all_dataset.log
-Rscript $HOME/MS-Thesis/code/networks/make_all_dataset.R
+#SBATCH --output=longitudinal_mediation_dataset.log
+Rscript $HOME/MS-Thesis/code/networks/make_longitudinal_mediation_dataset.R
