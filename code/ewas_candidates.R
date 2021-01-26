9c0b8d3f2c1ade4701c648f070f0bdf1c62bbe94
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-01-26 12:28:26 -0700

    Update ewas_candidates.R

--- code/ewas_candidates.R
+++ code/ewas_candidates.R
@@ -55,28 +55,51 @@ length(cereal_candidates) = n
 late_infancy_candidates = 
   bind_cols(bfdur_candidates,dairy_candidates,egg_candidates,meat_candidates,
             veg_candidates,fruit_candidates,gluten_candidates,cereal_candidates)
-# # Childhood
-# # BF duration
-# load("./bfdur_infancy_mods.RData")
-# bfdur_candidates = get_candidates(bfdur_infancy_mods,0.05)
-# # Dairy
-# load("./dairy_infancy_mods.RData")
-# dairy_candidates = get_candidates(dairy_infancy_mods,0.05)
-# # Egg
-# load("./egg_infancy_mods.RData")
-# egg_candidates = get_candidates(egg_infancy_mods,0.05)
-# # Meat
-# load("./meat_infancy_mods.RData")
-# meat_candidates = get_candidates(meat_infancy_mods,0.05)
-# # Veg
-# load("./veg_infancy_mods.RData")
-# veg_candidates = get_candidates(veg_infancy_mods,0.05)
-# # Fruit
-# load("./fruit_infancy_mods.RData")
-# fruit_candidates = get_candidates(fruit_infancy_mods,0.05)
-# # Gluten
-# load("./gluten_infancy_mods.RData")
-# gluten_candidates = get_candidates(gluten_infancy_mods,0.05)
-# # Cereal
-# load("./cereal_infancy_mods.RData")
-# cereal_candidates = get_candidates(cereal_infancy_mods,0.05)
+colnames(late_infancy_candidates) = c("bfdur","dairy","egg","meat","veg",
+                                      "fruit","gluten","cereal")
+write.csv(late_infancy_candidates,"late_infancy_candidates.csv",row.names = F,
+          na = "")
+# Childhood
+# BF duration
+load("./bfdur_childhood_mods.RData")
+bfdur_candidates = get_candidates(bfdur_childhood_mods,0.05)
+# Dairy
+load("./dairy_childhood_mods.RData")
+dairy_candidates = get_candidates(dairy_childhood_mods,0.05)
+# Egg
+load("./egg_childhood_mods.RData")
+egg_candidates = get_candidates(egg_childhood_mods,0.05)
+# Meat
+load("./meat_childhood_mods.RData")
+meat_candidates = get_candidates(meat_childhood_mods,0.05)
+# Veg
+load("./veg_childhood_mods.RData")
+veg_candidates = get_candidates(veg_childhood_mods,0.05)
+# Fruit
+load("./fruit_childhood_mods.RData")
+fruit_candidates = get_candidates(fruit_childhood_mods,0.05)
+# Gluten
+load("./gluten_childhood_mods.RData")
+gluten_candidates = get_candidates(gluten_childhood_mods,0.05)
+# Cereal
+load("./cereal_childhood_mods.RData")
+cereal_candidates = get_candidates(cereal_childhood_mods,0.05)
+# Candidate table
+n = max(length(bfdur_candidates),length(dairy_candidates),length(egg_candidates),
+        length(meat_candidates),length(veg_candidates),length(fruit_candidates),
+        length(gluten_candidates),length(cereal_candidates))
+length(bfdur_candidates) = n
+length(dairy_candidates) = n
+length(egg_candidates) = n
+length(meat_candidates) = n
+length(veg_candidates) = n
+length(fruit_candidates) = n
+length(gluten_candidates) = n
+length(cereal_candidates) = n
+late_childhood_candidates = 
+  bind_cols(bfdur_candidates,dairy_candidates,egg_candidates,meat_candidates,
+            veg_candidates,fruit_candidates,gluten_candidates,cereal_candidates)
+colnames(late_childhood_candidates) = c("bfdur","dairy","egg","meat","veg",
+                                      "fruit","gluten","cereal")
+write.csv(late_childhood_candidates,"late_childhood_candidates.csv",row.names = F,
+          na = "")
