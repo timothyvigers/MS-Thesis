99fd90f0858c96e1eaa989002328ec3bfc2ff638
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-01-31 13:38:48 -0700

    slurm

--- code/mediation/slurm_mediation.sh
+++ code/mediation/slurm_mediation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 24
+#SBATCH -n 20
 #SBATCH --job-name=tim_cits_mediation
 #SBATCH --output=tim_cits_mediation.log
-time Rscript $HOME/MS-Thesis/code/mediation/cits_mediation_quasi.R
+time Rscript $HOME/MS-Thesis/code/mediation/longitudinal_selection.R
