ea5256f9a334337d6ec8f8ed06f81dc7caa24bce
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-06 15:34:25 -0700

    Update longitudinal_selection.R

--- code/mediation/longitudinal_selection.R
+++ code/mediation/longitudinal_selection.R
@@ -4,18 +4,39 @@ load("./probesFromPipeline.Rdata")
 metab_candidates = read.csv("./liz_candidates.csv",stringsAsFactors = F,na.strings = "")
 metab_candidates = unlist(metab_candidates)
 metab_candidates = unique(metab_candidates[!is.na(metab_candidates)])
-# Model functions
-methyl_psv_mods = function(df,no_cores = 6) {
-  require(parallel)
-  # Make cluster
-  # cl = makeCluster(no_cores,type = "FORK")
-  # Models
-  candidates = lapply(probesFromPipeline, function(p){
-    
-    t = df[]
-    methyl = df[,p]
+# Methylation at PSV, metabolite at SV
+methyl_psv_candidates = lapply(probesFromPipeline, function(p){
+  candidates = lapply(metab_candidates, function(m){
+    ia = factor(psv$IAgroup2)
+    meth = psv[,p]
+    meta = sv[,m]
+    a = summary(lm(meta ~ meth))$coefficients[2,4]
+    b = summary(glm(ia ~ meta,family = binomial("logit")))$coefficients[2,4]
+    c = summary(glm(ia ~ meth,family = binomial("logit")))$coefficients[2,4]
+    if(a < 0.001 & b < 0.05 & c < 0.05){
+      return(c(p,m))
+    } else {return(c(NA,NA))}
   })
-  
-  
-  
-}
+  candidates = do.call(rbind,candidates)
+  candidates[complete.cases(candidates),]
+})
+methyl_psv_candidates = do.call(rbind,methyl_psv_candidates)
+save(methyl_psv_candidates,file = "./methyl_psv_candidates.Rdata")
+# Methylation at SV, metabolite at PSV
+metab_psv_candidates = lapply(probesFromPipeline, function(p){
+  candidates = lapply(metab_candidates, function(m){
+    ia = factor(psv$IAgroup2)
+    meth = sv[,p]
+    meta = psv[,m]
+    a = summary(lm(meta ~ meth))$coefficients[2,4]
+    b = summary(glm(ia ~ meta,family = binomial("logit")))$coefficients[2,4]
+    c = summary(glm(ia ~ meth,family = binomial("logit")))$coefficients[2,4]
+    if(a < 0.001 & b < 0.05 & c < 0.05){
+      return(c(p,m))
+    } else {return(c(NA,NA))}
+  })
+  candidates = do.call(rbind,candidates)
+  candidates[complete.cases(candidates),]
+})
+metab_psv_candidates = do.call(rbind,metab_psv_candidates)
+save(metab_psv_candidates,file = "./metab_psv_candidates.Rdata")
