fb28016d0e4f25bb4def0b1094d1920a38580a4d
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-06 14:39:32 -0700

    Update longitudinal_selection.R

--- code/mediation/longitudinal_selection.R
+++ code/mediation/longitudinal_selection.R
@@ -1,116 +1,21 @@
-# Data import
-# Phenotype
-pheno = read.csv("/home/biostats_share/Norris/data/phenotype/ivyomicssample_noIdentifyingInfo.csv",stringsAsFactors = F,na.strings = "")
-# Probes
-load("/home/biostats_share/Norris/data/methylation/probesFromPipeline.Rdata")
-# Methylation
-load("/home/biostats_share/Norris/data/methylation/Mmatrix.platformAdj.regressOut.Rdata")
-methyl = as.data.frame(t(M.adj))
-# Keys
-key_450k = read.csv("/home/biostats_share/Norris/data/methylation/key.450K.csv",
-                     stringsAsFactors = F)
-key_450k$platform = "450K"
-key_epic = read.csv("/home/biostats_share/Norris/data/methylation/key.EPIC.csv",
-                     stringsAsFactors = F)
-key_epic$platform = "EPIC"
-key = rbind(key_450k,key_epic)
-# Make final methylation dataset
-methyl_samplekey = key$samplekey[match(rownames(methyl),key$array)]
-rm("key","key_450k","key_epic")
-# PSV and SV samples
-psv_samples = pheno$samplekey[which(pheno$Visit_Type == "PSV")]
-sv_samples = pheno$samplekey[which(pheno$Visit_Type == "SV")]
-# Model function
-run_mods = function(methyl,metab,no_cores = 20) {
+setwd("C:/Users/Tim Vigers/Documents/GitHub/MS-Thesis/data/raw_data")
+load("./psv_sv_dataset.Rdata")
+load("./probesFromPipeline.Rdata")
+metab_candidates = read.csv("./liz_candidates.csv",stringsAsFactors = F,na.strings = "")
+metab_candidates = unlist(metab_candidates)
+metab_candidates = unique(metab_candidates[!is.na(metab_candidates)])
+# Model functions
+methyl_psv_mods = function(df,no_cores = 6) {
   require(parallel)
-  # Match indices
-  psv_methyl_match = match(psv_samples,methyl_samplekey)
-  psv_metab_match = match(psv_samples,rownames(metab))
-  sv_methyl_match = match(sv_samples,methyl_samplekey)
-  sv_metab_match = match(sv_samples,rownames(metab))
-  ia = factor(pheno$IAgroup2)
   # Make cluster
-  cl = makeCluster(no_cores,type = "FORK")
-  # PSV
-  psv_candidates = parLapply(cl,names(methyl), function(c){
-    # Check association between methylation and IA
-    meth = methyl[,c]
-    meth_mod = lm(meth[psv_methyl_match] ~ ia[match(psv_samples,pheno$samplekey)])
-    metab_mods = lapply(names(metab), function(m){
-      # Check association between metabolite and IA
-      meta = metab[,m]
-      meta_mod = lm(meta[psv_metab_match] ~ ia[match(psv_samples,pheno$samplekey)])
-      # Check association between metabolite and methylation
-      mm_mod = lm(meth[psv_methyl_match] ~ meta[psv_metab_match])
-      if(summary(meta_mod)$coefficients[2,4] > 0.05 | 
-         summary(meth_mod)$coefficients[2,4] > 0.05) {
-        return(NA)
-      } else if(summary(mm_mod)$coefficients[2,4] < 0.01) {
-        return(paste(c,m))
-      } else {
-          return(NA)
-        }
-    })
+  # cl = makeCluster(no_cores,type = "FORK")
+  # Models
+  candidates = lapply(probesFromPipeline, function(p){
+    
+    t = df[]
+    methyl = df[,p]
   })
-  psv_candidates = unlist(psv_candidates)[!is.na(unlist(psv_candidates))]
-  # SV
-  sv_candidates = parLapply(cl,names(methyl), function(c){
-    # Check association between methylation and IA
-    meth = methyl[,c]
-    meth_mod = lm(meth[sv_methyl_match] ~ ia[match(sv_samples,pheno$samplekey)])
-    metab_mods = lapply(names(metab), function(m){
-      # Check association between metabolite and IA
-      meta = metab[,m]
-      meta_mod = lm(meta[sv_metab_match] ~ ia[match(sv_samples,pheno$samplekey)])
-      # Check association between metabolite and methylation
-      mm_mod = lm(meth[sv_methyl_match] ~ meta[sv_metab_match])
-      if(summary(meta_mod)$coefficients[2,4] > 0.05 | 
-         summary(meth_mod)$coefficients[2,4] > 0.05) {
-        return(NA)
-      } else if(summary(mm_mod)$coefficients[2,4] < 0.01) {
-        return(paste(c,m))
-      } else {
-        return(NA)
-      }
-    })
-  })
-  sv_candidates = unlist(sv_candidates)[!is.na(unlist(sv_candidates))]
-  stopCluster(cl)
-  # Results
-  return(intersect(psv_candidates,sv_candidates))
+  
+  
+  
 }
-# gctof
-# gctof = read.csv("/home/biostats_share/Norris/data/metabolomics/gctof.bc.csv")
-# rownames(gctof) = gctof$samplekey
-# gctof$samplekey = NULL
-# gctof_candidates = run_mods(methyl = methyl,metab = gctof)
-# save(gctof_candidates,file = "/home/vigerst/MS-Thesis/data/mediation/gctof_candidates.RData")
-# rm("gctof")
-# hilic
-hilic = read.csv("/home/biostats_share/Norris/data/metabolomics/hilic.bc.csv")
-rownames(hilic) = hilic$samplekey
-hilic$samplekey = NULL
-hilic_candidates = run_mods(methyl = methyl,metab = hilic)
-save(hilic_candidates,file = "/home/vigerst/MS-Thesis/data/mediation/hilic_candidates.RData")
-rm("hilic")
-# lipid
-lipid = read.csv("/home/biostats_share/Norris/data/metabolomics/lipid.bc.csv")
-rownames(lipid) = lipid$samplekey
-lipid$samplekey = NULL
-lipid_candidates = run_mods(methyl = methyl,metab = lipid)
-save(lipid_candidates,file = "/home/vigerst/MS-Thesis/data/mediation/lipid_candidates.RData")
-rm("lipid")
-# oxylipin
-oxylipin = read.csv("/home/biostats_share/Norris/data/metabolomics/oxylipin.bc.csv")
-oxylipin = oxylipin[!duplicated(oxylipin$samplekey),]
-rownames(oxylipin) = oxylipin$samplekey
-oxylipin$samplekey = NULL
-oxylipin_candidates = run_mods(methyl = methyl,metab = oxylipin)
-save(oxylipin_candidates,file = "/home/vigerst/MS-Thesis/data/mediation/oxylipin_candidates.RData")
-rm("oxylipin")
-# vitd
-vitd = read.csv("/home/biostats_share/Norris/data/metabolomics/vitD.bc.csv")
-rownames(vitd) = vitd$samplekey
-vitd$samplekey = NULL
-vitd_candidates = run_mods(methyl = methyl,metab = vitd)
-save(vitd_candidates,file = "/home/vigerst/MS-Thesis/data/mediation/vitd_candidates.RData")
