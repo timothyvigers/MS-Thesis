19f4d6a272a9a6e87e185e306c4ca301dd7436b2
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-08 09:57:28 -0700

    mediation test

--- code/mediation/mediation_boot.R
+++ code/mediation/mediation_boot.R
@@ -1,7 +1,8 @@
 library(mediation)
+library(parallel)
 set.seed(1017)
 # Load data
-setwd("/Users/timvigers/GitHub/MS-Thesis")
+setwd("/home/vigerst/MS-Thesis")
 load("./data/raw_data/methyl_psv_candidates.Rdata")
 load("./data/raw_data/psv_sv_dataset.Rdata")
 # Outcome and adjustment variables
@@ -9,8 +10,10 @@ ia = factor(psv$IAgroup2)
 clinage = psv$clinage
 SEX = factor(psv$SEX)
 dr34 = factor(psv$dr34,labels = c("No","Yes"))
+# Cluster
+cl = makeCluster(8,type = "FORK")
 # Iterate through all
-mediation_mods = apply(methyl_psv_candidates,1,function(r){
+mediation_mods = parApply(cl,methyl_psv_candidates,1,function(r){
   methyl = psv[,r[1]]
   metab = sv[,r[2]]
   # Mediation models
@@ -18,5 +21,6 @@ mediation_mods = apply(methyl_psv_candidates,1,function(r){
   c = glm(ia ~ methyl+metab+clinage+SEX+dr34,family = binomial("logit"))
   med = mediate(m,c,treat="methyl",mediator="metab",boot = T,sims = 50,long = F)
 }) 
+stopCluster(cl)
 # Save
 save(mediation_mods,file = "./data/mediation/longitudinal_mediation_mods.Rdata")
