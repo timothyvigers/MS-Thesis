0a5dca79591c5c09ce20e5e4e4e51097075ab754
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-09 14:31:43 -0700

    longitudinal parallel selection

--- code/mediation/longitudinal_selection.R
+++ code/mediation/longitudinal_selection.R
@@ -1,11 +1,16 @@
-setwd("C:/Users/Tim Vigers/Documents/GitHub/MS-Thesis/data/raw_data")
+library(parallel)
+setwd("C:/Users/Tim Vigers/Dropbox/School/MS Thesis/data/raw_data")
 load("./psv_sv_dataset.Rdata")
 load("./probesFromPipeline.Rdata")
 metab_candidates = read.csv("./liz_candidates.csv",stringsAsFactors = F,na.strings = "")
 metab_candidates = unlist(metab_candidates)
 metab_candidates = unique(metab_candidates[!is.na(metab_candidates)])
+# Parallel
+n_cores = 8
+cl = makeCluster(n_cores)
+clusterExport(cl,list('metab_candidates','psv','sv','probesFromPipeline'))
 # Methylation at PSV, metabolite at SV
-methyl_psv_candidates = lapply(probesFromPipeline, function(p){
+methyl_psv_candidates = parLapply(cl,probesFromPipeline, function(p){
   candidates = lapply(metab_candidates, function(m){
     ia = factor(psv$IAgroup2)
     meth = psv[,p]
@@ -20,11 +25,15 @@ methyl_psv_candidates = lapply(probesFromPipeline, function(p){
   candidates = do.call(rbind,candidates)
   candidates[complete.cases(candidates),]
 })
+stopCluster(cl)
 methyl_psv_candidates = do.call(rbind,methyl_psv_candidates)
-save(methyl_psv_candidates,file = "./methyl_psv_candidates_p_0.05.Rdata")
+save(methyl_psv_candidates,file = "./methyl_psv_candidates_p_05.Rdata")
 rm("methyl_psv_candidates")
+# Restart cluster
+cl = makeCluster(n_cores)
+clusterExport(cl,list('metab_candidates','psv','sv','probesFromPipeline'))
 # Methylation at SV, metabolite at PSV
-metab_psv_candidates = lapply(probesFromPipeline, function(p){
+metab_psv_candidates = parLapply(cl,probesFromPipeline, function(p){
   candidates = lapply(metab_candidates, function(m){
     ia = factor(psv$IAgroup2)
     meth = sv[,p]
@@ -39,5 +48,6 @@ metab_psv_candidates = lapply(probesFromPipeline, function(p){
   candidates = do.call(rbind,candidates)
   candidates[complete.cases(candidates),]
 })
+stopCluster(cl)
 metab_psv_candidates = do.call(rbind,metab_psv_candidates)
-save(metab_psv_candidates,file = "./metab_psv_candidates_p_0.05.Rdata")
+save(metab_psv_candidates,file = "./metab_psv_candidates_p_05.Rdata")
