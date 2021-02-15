27fd4827102f9f95abfe5012272beab966620a0b
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-15 09:24:12 -0700

    adjust for change in age between PSV and SV

--- code/mediation/mediation_boot.R
+++ code/mediation/mediation_boot.R
@@ -3,13 +3,14 @@ library(parallel)
 set.seed(1017)
 # Load data
 setwd("/Users/timvigers/GitHub/MS-Thesis/")
-load("./data/raw_data/methyl_psv_candidates.Rdata")
+load("./data/mediation/methyl_psv_candidates_p_05.Rdata")
 load("./data/raw_data/psv_sv_dataset.Rdata")
 # Outcome and adjustment variables
 ia = factor(psv$IAgroup2)
-clinage = psv$clinage
 SEX = factor(psv$SEX)
 dr34 = factor(psv$dr34,labels = c("No","Yes"))
+# Change in age from PSV to SV
+age = sv$clinage - psv$clinage
 # Cluster
 cl = makeCluster(8,type = "FORK")
 # Iterate through all
@@ -17,10 +18,10 @@ mediation_mods = parApply(cl,methyl_psv_candidates,1,function(r){
   methyl = psv[,r[1]]
   metab = sv[,r[2]]
   # Mediation models
-  m = lm(metab~methyl+clinage+SEX+dr34)
-  c = glm(ia ~ methyl+metab+clinage+SEX+dr34,family = binomial("logit"))
+  m = lm(metab~methyl+age+SEX+dr34)
+  c = glm(ia ~ methyl+metab+age+SEX+dr34,family = binomial("logit"))
   med = mediate(m,c,treat="methyl",mediator="metab",boot = T,sims = 100000)
 }) 
 stopCluster(cl)
 # Save
-save(mediation_mods,file = "./data/mediation/longitudinal_mediation_mods_100k.Rdata")
+save(mediation_mods,file = "./data/mediation/longitudinal_mediation_p_05_100k_sims.Rdata")
