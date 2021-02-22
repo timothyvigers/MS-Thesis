732dc81ab593d01713431cf7bf18587ce9ae40af
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-22 14:53:04 -0700

    Update mediation_boot.R

--- code/mediation/mediation_boot.R
+++ code/mediation/mediation_boot.R
@@ -2,28 +2,33 @@ library(mediation)
 library(parallel)
 set.seed(1017)
 # Load data
-setwd("C:/Users/Tim Vigers/Dropbox/School/MS Thesis/")
+setwd("/Users/timvigers/Documents/School/MS Thesis/")
 load("./data/mediation/methyl_psv_candidates_p_05.Rdata")
 load("./data/raw_data/psv_sv_dataset.Rdata")
 # Outcome and adjustment variables
 ia = factor(psv$IAgroup2)
 SEX = factor(psv$SEX)
 dr34 = factor(psv$dr34,labels = c("No","Yes"))
-# Change in age from PSV to SV
-age = sv$clinage - psv$clinage
-# Cluster
-cl = makeCluster(8,type = "SOCK")
-clusterExport(cl,varlist = list("methyl_psv_candidates","psv","sv",
-                      "ia","SEX","dr34","age","mediate"))
-# Iterate through all
-mediation_mods = parApply(cl,methyl_psv_candidates[1:8,],1,function(r){
-  methyl = psv[,r[1]]
-  metab = sv[,r[2]]
-  # Mediation models
-  m = lm(metab~methyl+age+SEX+dr34)
-  c = glm(ia ~ methyl+metab+age+SEX+dr34,family = binomial("logit"))
-  med = mediate(m,c,treat="methyl",mediator="metab",boot = T,sims = 1000)
-})
-stopCluster(cl)
-# Save
-save(mediation_mods,file = "./data/mediation/longitudinal_mediation_p_05_100k_sims.Rdata")
+# Model function
+med_mods = function(age,out_name,n_cores = 8,n_sims = 10000){
+  # Cluster
+  cl = makeCluster(n_cores,type = "FORK")
+  # Iterate through all
+  mediation_mods = parApply(cl,methyl_psv_candidates,1,function(r){
+    methyl = psv[,r[1]]
+    metab = sv[,r[2]]
+    # Mediation models
+    m = lm(metab~methyl+age+SEX+dr34)
+    c = glm(ia ~ methyl+metab+age+SEX+dr34,family = binomial("logit"))
+    med = mediate(m,c,treat="methyl",mediator="metab",boot = T,sims = n_sims)
+  })
+  stopCluster(cl)
+  # Save
+  save(mediation_mods,file = paste0("./data/mediation/",out_name,"_",n_sims / 1000,"k_sims.Rdata"))
+}
+# Age at PSV
+med_mods(age = psv$clinage,out_name = "long_med_p_05_psv_age")
+# Time from PSV to SV
+med_mods(age = sv$clinage - psv$clinage,out_name = "long_med_p_05_delta_age")
+# Time from PSV to SV
+med_mods(age = psv$clinage - psv$clinage,out_name = "long_med_p_05_no_age")
