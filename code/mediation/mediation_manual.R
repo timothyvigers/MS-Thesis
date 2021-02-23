3bdcd7d8966e6c89c50654643150fc5bdf221dfe
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-23 10:29:30 -0700

    Update mediation_manual.R

--- code/mediation/mediation_manual.R
+++ code/mediation/mediation_manual.R
@@ -10,10 +10,27 @@ SEX = psv$SEX
 dr34 = psv$dr34
 covariates = as.data.frame(cbind(ia,SEX,dr34))
 covariates = as.data.frame(lapply(covariates,as.factor))
+# Bootstrap function
+boot_med = function(data,i){
+  d = data[i,] # allows boot to select sample
+  # Mediation models
+  # Because we have a case-control design, the mediator model should be fit to 
+  # only the controls to account for oversampling a rare outcome (VanderWeele pg. 28)
+  m = lm(metab~methyl+age+SEX+dr34,data = d[d$ia=="control",])
+  # Refit without interaction term
+  c = glm(ia ~ methyl+metab+age+SEX+dr34,data = d,family = binomial("logit"))
+  # Calculate direct and indirect effect (starting on VandwerWeele pg. 27)
+  de = summary(c)$coefficients[2,1]
+  ie = summary(m)$coefficients[2,1] * summary(c)$coefficients[3,1]
+  # Proportion mediated (VandwerWeele pg.48 and per Patrick Carry)
+  pmed = (exp(de)*(exp(ie)-1))/(exp(de)*exp(ie) - 1)
+  return(c(de,ie,pmed))
+}
 # Model function
 med_mods_tim = function(age,out_name,n_cores = 8,n_sims = 10000,long = F){
   # Iterate through all
-  mediation_results = for(row in 1:3{
+  mediation_results = list()
+  for(row in 1:3){
     r = methyl_psv_candidates[row,]
     methyl = psv[,r[1]]
     metab = sv[,r[2]]*1000 # Metabolite estimates are difficult to interpret on current scale
@@ -27,26 +44,21 @@ med_mods_tim = function(age,out_name,n_cores = 8,n_sims = 10000,long = F){
     c = glm(ia ~ methyl+metab+age+SEX+dr34,data = df,family = binomial("logit"))
     if (summary(c)$coefficients[2,4] > 0.05 | summary(c)$coefficients[3,4] > 0.05 |
         summary(m)$coefficients[2,4] > 0.05) {next}
-    # Bootstrap
-    boot_med = function(data,i){
-      d = data[i,] # allows boot to select sample
-      # Mediation models
-      # Because we have a case-control design, the mediator model should be fit to 
-      # only the controls to account for oversampling a rare outcome (VanderWeele pg. 28)
-      m = lm(metab~methyl+age+SEX+dr34,data = d[d$ia=="control",])
-      # Refit without interaction term
-      c = glm(ia ~ methyl+metab+age+SEX+dr34,data = d,family = binomial("logit"))
-      # Calculate direct and indirect effect (starting on VandwerWeele pg. 27)
-      de = summary(c)$coefficients[2,1]
-      ie = summary(m)$coefficients[2,1] * summary(c)$coefficients[3,1]
-      # Proportion mediated (VandwerWeele pg.48 and per Patrick Carry)
-      pmed = (exp(de)*(exp(ie)-1))/(exp(de)*exp(ie) - 1)
-      return(c(de,ie,pmed))
+    # Run bootstrap
+    res = boot(data = df,statistic = boot_med,R = n_sims,ncpus = n_cores)
+    # Confidence intervals
+    de_ci = boot.ci(res,type = "perc",index = 1)
+    de_ci = de_ci$percent[c(4,5)]
+    ie_ci = boot.ci(res,type = "perc",index = 2)
+    ie_ci = ie_ci$percent[c(4,5)]
+    pmed_ci = boot.ci(res,type = "perc",index = 3)
+    pmed_ci = pmed_ci$percent[c(4,5)]
+    # Return
+    ret = c(res$t0[1],de_ci,res$t0[2],ie_ci,res$t0[3],pmed_ci)
+    mediation_results[[row]] = ret
     }
-    res = boot(data = df,statistic = boot_med,R = 1000)
-  })
   # Save
-  save(mediation_mods,file = paste0("./data/mediation/",out_name,"_",n_sims / 1000,"k_sims.Rdata"))
+  save(mediation_results,file = paste0("./data/mediation/",out_name,"_manual.Rdata"))
 }
 # Age at PSV
 med_mods(age = psv$clinage,out_name = "long_med_p_05_psv_age")
