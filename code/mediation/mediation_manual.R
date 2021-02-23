c9a9d10a0509b6e2f6452b6890e5f7f62bc41472
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-23 10:07:50 -0700

    Update mediation_manual.R

--- code/mediation/mediation_manual.R
+++ code/mediation/mediation_manual.R
@@ -19,27 +19,31 @@ med_mods_tim = function(age,out_name,n_cores = 8,n_sims = 10000,long = F){
     metab = sv[,r[2]]*1000 # Metabolite estimates are difficult to interpret on current scale
     # Dataframe for bootstrap
     df = as.data.frame(cbind(methyl,metab,age,covariates))
-    # Mediation models
-    # Because we have a case-control design, the mediator model should be fit to 
-    # only the controls to account for oversampling a rare outcome (VanderWeele pg. 28)
-    m = lm(metab~methyl+age+SEX+dr34,data = df[df$ia=="control",])
     # Check interaction effect - skip any with interaction for now
     c = glm(ia ~ methyl*metab+age+SEX+dr34,data = df,family = binomial("logit"))
     if (summary(c)$coefficients[7,4] < 0.05){next}
-    # Refit without interaction term
-    c = glm(ia ~ methyl+metab+age+SEX+dr34,data = df,family = binomial("logit"))
     # Check Baron and Kenny steps
+    m = lm(metab~methyl+age+SEX+dr34,data = df[df$ia=="control",])
+    c = glm(ia ~ methyl+metab+age+SEX+dr34,data = df,family = binomial("logit"))
     if (summary(c)$coefficients[2,4] > 0.05 | summary(c)$coefficients[3,4] > 0.05 |
         summary(m)$coefficients[2,4] > 0.05) {next}
-    # Calculate direct and indirect effect (starting on VandwerWeele pg. 27)
-    de = summary(c)$coefficients[2,1]
-    ie = summary(m)$coefficients[2,1] * summary(c)$coefficients[3,1]
-    # Proportion mediated (VandwerWeele pg.48 and per Patrick Carry)
-    pmed = (exp(de)*(exp(ie)-1))/(exp(de)*exp(ie) - 1)
     # Bootstrap
-    boot_med = function(df,i){
-      
+    boot_med = function(data,i){
+      d = data[i,] # allows boot to select sample
+      # Mediation models
+      # Because we have a case-control design, the mediator model should be fit to 
+      # only the controls to account for oversampling a rare outcome (VanderWeele pg. 28)
+      m = lm(metab~methyl+age+SEX+dr34,data = d[d$ia=="control",])
+      # Refit without interaction term
+      c = glm(ia ~ methyl+metab+age+SEX+dr34,data = d,family = binomial("logit"))
+      # Calculate direct and indirect effect (starting on VandwerWeele pg. 27)
+      de = summary(c)$coefficients[2,1]
+      ie = summary(m)$coefficients[2,1] * summary(c)$coefficients[3,1]
+      # Proportion mediated (VandwerWeele pg.48 and per Patrick Carry)
+      pmed = (exp(de)*(exp(ie)-1))/(exp(de)*exp(ie) - 1)
+      return(c(de,ie,pmed))
     }
+    res = boot(data = df,statistic = boot_med,R = 1000)
   })
   # Save
   save(mediation_mods,file = paste0("./data/mediation/",out_name,"_",n_sims / 1000,"k_sims.Rdata"))
