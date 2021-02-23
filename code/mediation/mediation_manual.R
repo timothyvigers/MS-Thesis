48af9654190796aa66f366f511c0dfb5c0711696
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-02-23 09:58:59 -0700

    Update mediation_manual.R

--- code/mediation/mediation_manual.R
+++ code/mediation/mediation_manual.R
@@ -1,31 +1,43 @@
-library(mediation)
-library(parallel)
+library(boot)
 set.seed(1017)
 # Load data
 setwd("/Users/timvigers/Documents/School/MS Thesis/")
 load("./data/mediation/methyl_psv_candidates_p_05.Rdata")
 load("./data/raw_data/psv_sv_dataset.Rdata")
 # Outcome and adjustment variables
-ia = factor(psv$IAgroup2)
-SEX = factor(psv$SEX)
-dr34 = factor(psv$dr34,labels = c("No","Yes"))
-controls = which(psv$IAgroup2 == "control")
+ia = psv$IAgroup2
+SEX = psv$SEX
+dr34 = psv$dr34
+covariates = as.data.frame(cbind(ia,SEX,dr34))
+covariates = as.data.frame(lapply(covariates,as.factor))
 # Model function
 med_mods_tim = function(age,out_name,n_cores = 8,n_sims = 10000,long = F){
   # Iterate through all
-  mediation_results = apply(methyl_psv_candidates,1,function(r){
+  mediation_results = for(row in 1:3{
+    r = methyl_psv_candidates[row,]
     methyl = psv[,r[1]]
-    metab = sv[,r[2]]
+    metab = sv[,r[2]]*1000 # Metabolite estimates are difficult to interpret on current scale
+    # Dataframe for bootstrap
+    df = as.data.frame(cbind(methyl,metab,age,covariates))
     # Mediation models
     # Because we have a case-control design, the mediator model should be fit to 
     # only the controls to account for oversampling a rare outcome (VanderWeele pg. 28)
-    m = lm(metab[controls]~methyl[controls]+age[controls]+SEX[controls]+dr34[controls])
-    c = glm(ia ~ methyl*metab+age+SEX+dr34,family = binomial("logit"))
+    m = lm(metab~methyl+age+SEX+dr34,data = df[df$ia=="control",])
+    # Check interaction effect - skip any with interaction for now
+    c = glm(ia ~ methyl*metab+age+SEX+dr34,data = df,family = binomial("logit"))
+    if (summary(c)$coefficients[7,4] < 0.05){next}
+    # Refit without interaction term
+    c = glm(ia ~ methyl+metab+age+SEX+dr34,data = df,family = binomial("logit"))
+    # Check Baron and Kenny steps
+    if (summary(c)$coefficients[2,4] > 0.05 | summary(c)$coefficients[3,4] > 0.05 |
+        summary(m)$coefficients[2,4] > 0.05) {next}
     # Calculate direct and indirect effect (starting on VandwerWeele pg. 27)
     de = summary(c)$coefficients[2,1]
     ie = summary(m)$coefficients[2,1] * summary(c)$coefficients[3,1]
+    # Proportion mediated (VandwerWeele pg.48 and per Patrick Carry)
+    pmed = (exp(de)*(exp(ie)-1))/(exp(de)*exp(ie) - 1)
     # Bootstrap
-    boot_med = function(df){
+    boot_med = function(df,i){
       
     }
   })
