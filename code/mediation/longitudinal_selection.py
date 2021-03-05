c77ca7e6d205fc07a575601ab582079873c695a8
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-03-04 18:28:40 -0700

    round 2!

--- code/mediation/longitudinal_selection.py
+++ code/mediation/longitudinal_selection.py
@@ -42,4 +42,32 @@ for i in range(0,len(probe_candidates)):
             methyl_psv_candidates.append([p,m])
 # Write results
 pd.DataFrame(methyl_psv_candidates).to_csv(wd+"mediation/methyl_psv_py.csv",index=False)
-
+# Same again, but switch psv and sv
+# Check probe association with outcome
+probe_candidates = [] # To store results
+for i in range(0,len(probes)):
+    c = probes[i][0]
+    logit_model=sm.Logit(ia,sm.add_constant(psv[[c]]))
+    result=logit_model.fit(disp=0)
+    if result.pvalues[[1]][0] < 0.5:
+        probe_candidates.append(c)
+# Now check metabolite association with outcome
+metab_candidates = [] # To store results
+for i in range(0,len(metabolites)):
+    m = metabolites[i][0]
+    logit_model=sm.Logit(ia,sm.add_constant(sv[[m]]))
+    result=logit_model.fit(disp=0)
+    if result.pvalues[[1]][0] < 0.5:
+        metab_candidates.append(m)
+# Check which pairs are associated with each other
+methyl_psv_candidates = []
+for i in range(0,len(probe_candidates)):
+    p = probe_candidates[i]
+    for j in range(0,len(metab_candidates)):
+        m = metab_candidates[j]
+        model=sm.OLS(sv[[m]],sm.add_constant(psv[[p]]))
+        result=model.fit(disp=0)
+        if result.pvalues[[1]][0] < 0.5:
+            methyl_psv_candidates.append([p,m])
+# Write results
+pd.DataFrame(methyl_psv_candidates).to_csv(wd+"mediation/methyl_psv_py.csv",index=False)
