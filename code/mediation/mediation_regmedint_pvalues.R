f602d1167b544c0c4c3f16169a488a4e92bf11d4
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-04-27 14:08:50 -0600

    Create mediation_regmedint_pvalues.R

---
+++ code/mediation/mediation_regmedint_pvalues.R
@@ -0,0 +1,80 @@
+library(regmedint)
+library(parallel)
+set.seed(1017)
+# Load data
+#setwd("/home/vigerst/MS-Thesis/")
+setwd("~/Dropbox/School/MS Thesis")
+load("./data/raw_data/probesFromPipeline.Rdata")
+load("./data/raw_data/psv_sv_dataset.Rdata")
+load("./data/mediation/methyl_psv_candidates_p_01.Rdata")
+load("./data/mediation/metab_psv_candidates_p_01.Rdata")
+# Outcome and adjustment variables
+ia = as.numeric(factor(psv$IAgroup2)) - 1
+SEX = as.numeric(factor(psv$SEX)) - 1
+dr34 = psv$dr34
+age_delta = as.numeric(sv$clinage) - as.numeric(psv$clinage)
+age = as.numeric(psv$clinage)
+covariates = as.data.frame(cbind(ia,SEX,dr34,age,age_delta))
+# Mediation function
+regmed = function(d){
+  regmedint_obj = 
+    regmedint(data = d,
+              ## Variables
+              yvar = "ia",
+              avar = "methyl",
+              mvar = "metab",
+              cvar = c("SEX","dr34","age","age_delta"),
+              ## Values at which effects are evaluated
+              a0 = 0,
+              a1 = 1,
+              m_cde = 1,
+              c_cond = c(1,1,1,1),
+              ## Model types
+              mreg = "linear",
+              yreg = "logistic",
+              ## Additional specification
+              interaction = T,
+              casecontrol = T)
+  m = summary(regmedint_obj)$summary_myreg
+  m["tnie","p"]
+}
+# Iterate through all - parallel
+cores = 4
+cl = makeCluster(cores,type = "FORK")
+methyl_psv_pvalues = parApply(cl,methyl_psv_candidates,1,function(r){
+  methyl = as.character(r[1])
+  methyl = as.numeric(scale(psv[,methyl]))
+  metab = as.character(r[2])
+  metab = as.numeric(scale(sv[,metab]))
+  # Dataframe 
+  df = as.data.frame(cbind(methyl,metab,covariates))
+  df = df[complete.cases(df),]
+  # Mediation
+  med = regmed(df)
+  return(med)
+})
+methyl_psv_pvalues = as.data.frame(methyl_psv_pvalues)
+rownames(methyl_psv_pvalues) = apply(methyl_psv_candidates,1,paste,collapse = " & ")
+# Save
+save(methyl_psv_pvalues,file = "./data/mediation/methyl_psv_candidate_pvalues.Rdata")
+stopCluster(cl)
+# Same again for metab at PSV
+# Iterate through all - parallel
+cl = makeCluster(cores,type = "FORK")
+metab_psv_pvalues = parApply(cl,metab_psv_candidates,1,function(r){
+  methyl = as.character(r[1])
+  methyl = as.numeric(scale(psv[,methyl]))
+  metab = as.character(r[2])
+  metab = as.numeric(scale(sv[,metab]))
+  # Dataframe 
+  df = as.data.frame(cbind(methyl,metab,covariates))
+  df = df[complete.cases(df),]
+  # Mediation
+  med = regmed(df)
+  return(med)
+})
+metab_psv_pvalues = as.data.frame(metab_psv_pvalues)
+rownames(metab_psv_pvalues) = apply(metab_psv_candidates,1,paste,collapse = " & ")
+# Save
+save(metab_psv_pvalues,file = "./data/mediation/metab_psv_candidate_pvalues.Rdata")
+stopCluster(cl)
