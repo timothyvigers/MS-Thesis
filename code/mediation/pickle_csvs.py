45532329142e7128745d225629b76794ef6d3c03
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-04-27 14:17:06 -0600

    delete old code

--- code/mediation/pickle_csvs.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("/Users/timvigers/Dropbox/School/MS Thesis/data/raw_data/psv.csv")
-df.to_pickle("/Users/timvigers/Dropbox/School/MS Thesis/data/raw_data/psv.pkl")
-df = pd.read_csv("/Users/timvigers/Dropbox/School/MS Thesis/data/raw_data/sv.csv")
-df.to_pickle("/Users/timvigers/Dropbox/School/MS Thesis/data/raw_data/sv.pkl")
