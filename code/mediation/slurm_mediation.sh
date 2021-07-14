f716c8728325141c7290c95580d3082f327f703d
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-07-13 22:11:28 -0600

    Update slurm_mediation.sh

--- code/mediation/slurm_mediation.sh
+++ code/mediation/slurm_mediation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -n 12
+#SBATCH -n 16
 #SBATCH --job-name=tim_mediation
 #SBATCH --output=tim_mediation.log
-time Rscript $HOME/MS-Thesis/code/mediation/mediation_regmedint.R
+time Rscript $HOME/MS-Thesis/code/mediation/mediation_regmedint_boot.R
