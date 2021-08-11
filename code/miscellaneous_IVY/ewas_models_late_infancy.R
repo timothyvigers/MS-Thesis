ca68a6b93b4b93b96be83e87115aacd50254e677
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-08-11 14:58:51 -0600

    adjust late infancy for NHW, use 4-5 as reference group

--- code/miscellaneous_IVY/ewas_models_late_infancy.R
+++ code/miscellaneous_IVY/ewas_models_late_infancy.R
@@ -17,6 +17,7 @@ Mono = late_infancy$Mono
 Gran = late_infancy$Gran # drop to make independent
 ID = late_infancy$ID
 Platform = factor(late_infancy$Data)
+NHW = late_infancy$NHW
 # Fix continuous variables
 month_vars = c("frstdairy","frstwbr","frstro","frstsolidfruit","frstveg","frstmeat","frstwheat","frstbarley")
 late_infancy[,month_vars] = lapply(late_infancy[,month_vars],function(c){
@@ -29,15 +30,23 @@ exbf = late_infancy$exbf
 bfdur = late_infancy$bfdur
 bfwhbar = factor(late_infancy$bfwhbar,levels = c("n","y"),labels = c("N","Y"))
 frstdairy = late_infancy$frstdairy
+# For 3 group factors, make 4-5 months the reference group
 id_solidfood = factor(late_infancy$id_solidfood,labels = labs)
+id_solidfood = relevel(id_solidfood,ref = "4-5 months")
 id_cereal = factor(late_infancy$id_cereal,labels = labs)
+id_cereal = relevel(id_cereal,ref = "4-5 months")
 id_wbr = factor(late_infancy$id_wbr,labels = labs)
+id_wbr = relevel(id_wbr,ref = "4-5 months")
 id_wbr6mon = cut(late_infancy$frstwbr,c(-Inf,6,Inf),right = F,
                  labels = c("<6 months",">=6 months"))
 id_riceoat = factor(late_infancy$id_riceoat,labels = labs)
+id_riceoat = relevel(id_riceoat,ref = "4-5 months")
 id_solidfruit = factor(late_infancy$id_solidfruit,labels = labs)
+id_solidfruit = relevel(id_solidfruit,ref = "4-5 months")
 id_veg = factor(late_infancy$id_veg,labels = labs)
+id_veg = relevel(id_veg,ref = "4-5 months")
 id_meat = factor(late_infancy$id_meat,labels = labs)
+id_meat = relevel(id_meat,ref = "4-5 months")
 id_meat6mon = cut(late_infancy$frstmeat,c(-Inf,6,Inf),right = F,
                   labels = c("<6 months",">=6 months"))
 # List of variables
@@ -48,7 +57,7 @@ analysis_vars = c("exbf","bfdur","bfwhbar","frstdairy","id_solidfood","id_cereal
 cores = 16
 # Model function
 mod_fun = function(m,var){
-  mod = try(lm(m ~ var + Age + Sex + CD8T +	CD4T +	NK +	Bcell +	Mono + Platform))
+  mod = try(lm(m ~ var + Age + Sex + CD8T +	CD4T +	NK +	Bcell +	Mono + Platform + NHW))
   if(class(mod)=="lm"){
     return(summary(mod)$coefficients)
   } else {return(NA)}
