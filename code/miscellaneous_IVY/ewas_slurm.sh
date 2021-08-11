f997d4da0b47fb8e1eec6f33e13f898c6b3f0fef
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-08-11 15:02:17 -0600

    updated code path

--- code/miscellaneous_IVY/ewas_slurm.sh
+++ code/miscellaneous_IVY/ewas_slurm.sh
@@ -2,4 +2,4 @@
 #SBATCH -n 16
 #SBATCH --job-name=tim_ewas_LI
 #SBATCH --output=tim_ewas_LI.log
-time Rscript $HOME/MS-Thesis/code/ewas_models_late_infancy.R
+time Rscript $HOME/MS-Thesis/code/miscellaneous_IVY/ewas_models_late_infancy.R
