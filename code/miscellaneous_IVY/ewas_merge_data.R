6d4f13f2cb0d619e9a364d39375ffe77fbd259bb
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-09-07 19:00:58 -0600

    updated dataset

--- code/miscellaneous_IVY/ewas_merge_data.R
+++ code/miscellaneous_IVY/ewas_merge_data.R
@@ -2,7 +2,7 @@ setwd("/home/vigerst/EWAS")
 # Import original data
 load("./data/Mmatrix.platformAdj.regressOut.Rdata")
 load("./data/probesFromPipeline.Rdata")
-pheno = read.csv("./data/Infant_Diet_Methylation_Phenotype_Final_19MARCH2021.csv")
+pheno = read.csv("./data/Infant_Diet_Methylation_Phenotype_Final_7SEPT2021.csv")
 # Subset methylation data
 M.adj = M.adj[,colnames(M.adj) %in% pheno$Array]
 # Merge
